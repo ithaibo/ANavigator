d178a8edac9288913b1925437f886c775f4365df
Andy <hibowu30@gmail.com>
2019-04-05 23:14:41 +0800

    [refactor]:{add stub class Uri, Intent}

---
+++ android_stub/src/main/java/android/android/net/Uri.java
@@ -0,0 +1,5 @@
+package android.android.net;
+
+import android.os.Parcelable;
+
+public abstract class Uri implements Parcelable, Comparable<Uri> {}
--- android_stub/src/main/java/android/content/Intent.java
+++ android_stub/src/main/java/android/content/Intent.java
@@ -1,4 +1,29 @@
 package android.content;
 
+import android.android.net.Uri;
+
 public class Intent {
+    public Intent() {
+    }
+
+    /**
+     * Copy constructor.
+     */
+    public Intent(Intent o) {
+    }
+
+    private Intent(Intent o, int copyMode) {
+    }
+
+    public Intent(String action) {
+    }
+
+    public Intent(String action, Uri uri) {
+    }
+
+    public Intent(Context packageContext, Class<?> cls) {
+    }
+
+    public Intent(String action, Uri uri, Context packageContext, Class<?> cls) {
+    }
 }
